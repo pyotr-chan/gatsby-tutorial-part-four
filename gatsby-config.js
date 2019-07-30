0df9f20e5f7786ffe6597556a0fef021280641f1
pyotr_chan <pollyfeb@outlook.com>
2019-07-30 12:41:16 +0900

    install gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,7 @@ module.exports = {
       `gatsby-transformer-remark`,
       `gatsby-plugin-emotion`,
       `gatsby-plugin-offline`,
+      `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-typography`,
       options: {
